86f88f973770ae08450e59b82c4cbfeffa06e1e1
Jeff <jzinger100@gmail.com>
2017-02-08 16:30:20 +0100

    categories, random, search page created, categories links set up

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,59 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.use(expressLayouts);
+app.set('layout','layouts/main-layout');
+
+app.set('views', __dirname + "/views");
+app.set('view engine','ejs');
+
+app.use(express.static('public'))
+
+client.getJokeCategories()
+  .then((response)=>  {
+	console.log(response);
+	let data = {
+		categories: response
+	}
+     // use the response here
+		 app.get('/categories', (req, res) => {
+		   res.render('categories',data);
+		 });
+  })
+  .catch((err)=> {
+    console.error(err);
+  });
+
+	// Retrieve a random chuck joke from the given category
+	client.getRandomJoke('dev').then(function (response) {
+		// console.log(response.value);
+		let data = {
+			joke: response.value
+		}
+	    // to stuff here
+			app.get('/random', (req, res) => {
+ 		   res.render('random', data);
+ 		 });
+	}).catch(function (err) {
+	    // handle error
+	});
+
+const searchTerm = "kill";
+
+client.search(searchTerm).then(function (response) {
+	// console.log(response);
+    // to stuff here
+		app.get('/search', (req, res) => {
+		 res.render('search');
+	 });
+}).catch(function (err) {
+    // handle error
+});
+
+	// Server Started
+	app.listen(3000, () => {
+	  console.log('My first app listening on port 3000!')
+	});
