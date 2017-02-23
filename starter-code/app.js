cef1121b75d40ef03d8db8adceb7e53889bfa623
Marlon Martinez <jmarlonmartinez13@gmail.com>
2017-02-23 16:24:02 -0500

    inital setup

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,25 @@
 const express = require('express');
-const app = express();
 const Chuck  = require('chucknorris-io');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+
 const client = new Chuck();
 
+const myApp = express();
+
+myApp.set('views', __dirname + '/views');
+myApp.use(express.static('public'));
+myApp.set('view engine', 'ejs');
+myApp.use(expressLayouts);
+myApp.set('layout', 'layouts/main-layout');
+myApp.use(bodyParser.urlencoded({ extended: true }));
+
+
+myApp.get('/', (req, res, next ) => {
+  res.render('index');
+});
+
+
+myApp.listen(3000, () => {
+  console.log('Backend app ONLINE!');
+});
