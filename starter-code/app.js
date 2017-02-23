2f547a1a99e0074f3d634f26d6ac64b78710cdf4
Jeremy Beagle <beaglej2012@fau.edu>
2017-02-23 17:53:57 -0500

    All iterations complete

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,10 +2,20 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const Chuck  = require('chucknorris-io');
 const app = express();
+const bodyParser = require('body-parser');
 const client = new Chuck();
 
 app.set('views', __dirname + '/views'); //where the html files are
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/hi', (req, res, next) => {
+  res.render('hi');
+});
+
+app.get('/', (req, res, next) => {
+  res.render('home');
+});
 
 app.get('/random', (req, res, next) => {
   // Retrieve a random chuck joke
@@ -22,11 +32,6 @@ client.getRandomJoke()
   });
 });
 
-app.listen(3000, ()=>{
-  console.log('ONLINE!');
-});
-
-
 app.get('/categories', (req, res, next) => {
 
   client.getJokeCategories()
@@ -41,6 +46,29 @@ app.get('/categories', (req, res, next) => {
 
   });
 
+});
+
+app.get('/search', (req, res, next) => {
+  res.render('search-form', {
+
+  });
+});
+
+app.post('/search', (req, res, next) => {
+  const keyword = req.body.keyword;
+  client.search(keyword)
+
+  .then((response) => {
+
+    console.log(response);
+
+    res.send(`Joke: ${response.items[0].value}`);
+
+  }).catch((err) => {
+
+    res.send(err);
+
+  });
 });
 
 app.get('/:cat', (req, res, next) => {
@@ -59,8 +87,7 @@ app.get('/:cat', (req, res, next) => {
 
 });
 
-app.get('/search', (req, res, next) => {
-  res.render('search-form', {
 
-  });
+app.listen(3000, ()=>{
+  console.log('ONLINE!');
 });
