bbfcca979673381a2499372a3807291096c30854
Alina Gasperino <alina.gasperino@gmail.com>
2017-02-23 16:11:29 -0500

    Testing

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,32 @@
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(expressLayouts);
+app.set('layout', 'layouts/main-layout');
+
+
+
+app.get('/about', (req, res, next) => {
+  // display views/about.ejs for the browser
+  res.render('about', {
+    name: 'Nizar',
+    age: 30,
+    citiesTraveled: [ 'Miami', 'Madrid', 'Barcelona', 'Paris' ],
+    faveFoods: [
+      { name: 'pizza slice', calories: 400 },
+      { name: 'bagel bites', calories: 380 },
+      { name: 'pizza doritos', calories: 450 },
+    ]
+  });
+});
+
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!');
+});
