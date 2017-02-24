2abdbd6f188b3bb4f78c92073e5aff2fc30b7472
Marlon Martinez <jmarlonmartinez13@gmail.com>
2017-02-24 09:11:22 -0500

    almost done

--- starter-code/app.js
+++ starter-code/app.js
@@ -16,8 +16,64 @@ myApp.use(bodyParser.urlencoded({ extended: true }));
 
 
 myApp.get('/', (req, res, next ) => {
-  res.render('index');
+
+  client.getRandomJoke()
+  .then((response) => {
+    const joke = response.value;
+    res.render('index', {
+      joke: joke
+    });
+  }).catch((err) => {
+  });
 });
+
+myApp.get('/categories', (req, res, next ) => {
+
+  client.getJokeCategories()
+  .then((response)=>  {
+    const list = response;
+    res.render('categories', {
+      categories: list,
+    });
+  })
+  .catch((err)=> {
+    // handle error
+  });
+
+});
+
+myApp.get('/joke-by-category', (req, res, next) => {
+  const links = req.query.links;
+  client.getRandomJoke(links)
+  .then((response) => {
+    const jokeCategory = response.value;
+    res.render('joke-by-category', {
+      jokeCategory: jokeCategory
+    });
+  }).catch((err) => {
+
+  });
+});
+
+myApp.get('/search', (req, res, next) => {
+  res.render('search');
+});
+
+myApp.post('/search-joke', (req, res, next) => {
+  const user = req.body.search;
+
+  client.search(user).then((response) => {
+    const result = response.value;
+    res.render('search-result', {
+      searchResult: result
+    });
+
+  }).catch(function (err) {
+
+  });
+
+});
+
 
 
 myApp.listen(3000, () => {
