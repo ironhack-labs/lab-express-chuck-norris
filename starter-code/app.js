91b4402002b79d82f49dd597897dc9e4bce11a8b
Rafael Melón <info@rafaelmelon.es>
2017-04-26 16:03:20 +0200

    Primer commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,34 @@
+/* jshint esversion: 6 */
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+
+app.use(express.static('public'));
+app.use(expressLayouts);
+
+app.set('layout', 'layouts/main-layout');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+
+  client.getRandomJoke().then(function (response) {
+
+    res.render('index',{value:response.value});
+
+  }).catch(function (err) {
+
+    console.log("ERROR");
+
+  });
+
+});
+
+
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!');
+});
