e5a141fc6d7cbc2dfbedf94543d3fa9cd22bb156
Juampi <juanpabloloza@gmail.com>
2017-04-26 15:48:27 +0200

    add respond random joke and error

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,32 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+
+
+app.use(express.static('public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/random',(req,res,next)=>{
+
+ client.getRandomJoke()
+    .then((joke) => {
+      res.send(joke.value);
+    }).catch((err) => {
+      res.send(err);
+    });
+
+
+});
+
+
+
+
+
+
+
+
+app.listen(3002, () => {
+  console.log('My first app listening on port 3002!')
+});
