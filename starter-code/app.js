64df82c618d471a43b4a828295d25c61d92205f9
polgasull <pol89.gn@gmail.com>
2017-07-04 21:37:33 +0200

    starting

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,13 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res, next) => {
+    res.render("index");
+});
+
+app.listen(3001, () => {
+    console.log("listening!!!");
+});
