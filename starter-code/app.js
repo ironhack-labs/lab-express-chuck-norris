46f98841e27bb71ffd9d9242f7996f01bab1d4f2
Alejandro de la Fuente <alex695afr@gmail.com>
2017-07-05 19:50:12 +0200

    2

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,36 @@
+/*jshint esversion:6*/
 const express = require('express');
 const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+app.get('/random', (req, res) => {
+  client.getRandomJoke()
+    .then((resp) => {
+
+
+      console.log(resp);
+      res.send(`<p> <img src="${resp.iconUrl}"alt=""> ${resp.value} <a href="${resp.sourceUrl}">${resp.sourceUrl}</a></p> `);
+    }).catch((err) => {
+      console.log(err);
+  });
+});
+
+app.get('/categories', (req, res) => {
+  client.getJokeCategories()
+    .then((respo) => {
+      let array=respo;
+       res.render('categories',array);
+      console.log(respo);
+    }).catch((err) => {
+      console.log(err);
+  });
+});
+
+app.listen(3000, () => {
+  console.log("App is running!");
+});
