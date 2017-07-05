058ed9f0f9094baa9c18fd8a02db00503da619bd
mikelRumayor <mikel.rumayor@gmail.com>
2017-07-05 16:25:54 +0200

    iteration 1 done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,22 @@
-const express = require('express');
-const app = express();
-const Chuck  = require('chucknorris-io');
-const client = new Chuck();
+const express = require('express')
+const app = express()
+const Chuck  = require('chucknorris-io')
+const client = new Chuck()
 
+
+app.get('/random', (req, res) => {
+  let joke
+  client.getRandomJoke()
+    .then((response) => {
+      console.log(response.value)
+      joke = response.value
+      res.send(`<p> ${joke}</p>`)
+
+    }).catch((err) =>{
+
+    })
+})
+
+app.listen(3000, () => {
+  console.log('The server is listening in the port 3000')
+})
