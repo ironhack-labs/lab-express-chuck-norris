3fc91cd588029cd50c5e7521b4681169a7d2c2e7
mikelRumayor <mikel.rumayor@gmail.com>
2017-07-05 16:53:29 +0200

    iteration 2 done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,13 +8,35 @@ app.get('/random', (req, res) => {
   let joke
   client.getRandomJoke()
     .then((response) => {
-      console.log(response.value)
-      joke = response.value
-      res.send(`<p> ${joke}</p>`)
-
+      joke = response
+      res.send(`<img src="${joke.iconUrl}"/><p> ${joke.value}</p>`)
     }).catch((err) =>{
 
     })
+})
+
+app.get('/categories', (req, res) => {
+  let {cat} = req.query
+
+  if(cat) {
+    client.getRandomJoke(cat)
+      .then((response) => {
+        joke = response
+        res.render('joke-by-category.ejs', {category: cat, joke: joke})
+      }).catch((err) =>{
+
+      })
+  } else {
+    let categories
+    client.getJokeCategories()
+      .then((response) => {
+        categories = response
+
+        res.render('categories.ejs', {categories: categories})
+      }).catch((err) =>{
+
+      })
+  }
 })
 
 app.listen(3000, () => {
