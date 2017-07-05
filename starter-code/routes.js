e3e91bb207cd2decebb4accdae293ff7f66d0196
salvocostahack <salvatorecostanzopa@gmail.com>
2017-07-05 18:20:05 +0200

    iteration 2 done

---
+++ starter-code/routes.js
@@ -0,0 +1,58 @@
+const Chuck = require('chucknorris-io');
+const client = new Chuck();
+module.exports = (app) => {
+
+
+  app.get('/', (req, res) => {
+    res.render('index');
+  });
+
+
+  app.get('/random', (req, res) => {
+    client.getRandomJoke()
+      .then((response) => {
+        let joke = {
+          randomJoke: response.value
+        };
+        console.log(response);
+        res.render('random', joke);
+      }).catch((err) => {
+        console.log("Error in random jokes");
+      });
+
+  });
+
+
+  app.get('/categories', (req, res) => {
+    if(Object.keys(req.query).length === 0) {
+      client.getJokeCategories()
+        .then((response) => {
+          let jokesCategories = {
+            categories: response,
+          };
+            res.render('categories', jokesCategories);
+        })
+        .catch((err) => {
+          // handle error
+        });
+    } else {
+      let jokeReq = req.query;
+      console.log(jokeReq);
+      client.getRandomJoke(jokeReq)
+        .then((response) => {
+          let joke = {
+            randomJoke: response.value
+          };
+          console.log(response);
+          res.render('joke-by-category', joke);
+        }).catch((err) => {
+          console.log("Error in random jokes");
+        });
+    }
+  });
+
+  app.post('/search', (req, res) => {
+
+  });
+
+};
