e54f5c7c690a90f96ce1ee7756ee41bff8e175f3
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-09-06 17:00:07 +0200

    primera hecha

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,34 @@
-const express = require('express');
-const app = express();
-const Chuck  = require('chucknorris-io');
-const client = new Chuck();
+const express = require('express')
+const app = express()
+const Chuck = require('chucknorris-io')
+const client = new Chuck()
+const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
+app.use(expressLayouts)
+app.set('layout', 'index')
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+
+app.get('/random', (req, res) => {
+  client.getRandomJoke()
+    .then( response => {
+      res.render('random-joke', {
+        joke: response
+      })
+    }).catch((err) => {
+      console.log("Unable to get a random joke")
+    })
+})
+
+let port = 3000
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`)
+})
