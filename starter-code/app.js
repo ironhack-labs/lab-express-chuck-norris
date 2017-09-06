4a39108a89aac026b18bd25463daad210e056367
Mark Allen <mark@traversify.com>
2017-09-06 17:13:45 +0200

    NORRRIS

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,27 +1,64 @@
-const express = require('express');
-const app = express();
+const express         = require('express');
+const app             = express();
+const bodyParser      = require('body-parser');
+const expressLayouts  = require('express-ejs-layouts');
 app.use(express.static('public'));
-
-// const Chuck  = require('chucknorris-io');
-// const client = new Chuck();
-
-// middleware - app.use OR app.get/post/etc
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/main-layout');
+app.set('view engine', 'ejs');
+const Chuck  = require('chucknorris-io');
+const client = new Chuck();
 
 app.get('/', (req, res, next) => { 
-    console.log(req.method, req.path);
-    res.send(`<head> 
-                    <title>The Chuck Norris Joke Department</title> 
-                    <link rel="stylesheet" href="stylesheets/style.css"> 
-              </head> 
-              <body> 
-                    <div class="container"> 
-                        <h1>Hello Ironhack</h1> 
-                        <p>It is almost lunch time. Honestly, it is!</p> 
-                        <img src="images/messi.jpg" alt="jordis picture"> 
-                    </div> 
-                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-                    <script src="javascript/main.js"></script>
-              </body>`);
+    res.render('index');
 });
 
+app.get('/random', (req, res, next) => {
+     client.getRandomJoke().then((response) => {
+     res.render('random', response );
+}).catch((err) => {
+     throw err;
+  });
+     
+});
+
+
+app.get('/categories', (req, res, next) => { 
+    client.getJokeCategories().then((response)=>  {
+        res.render('categories', { categories: response });
+    })
+    .catch((err)=> {
+      throw err;
+    });
+    
+});
+
+
+app.get('/categories/:category', (req, res, next) => { 
+    client.getRandomJoke(req.params).then((response) => {
+        res.render('joke-by-category', response );
+   }).catch((err) => {
+        throw err;
+   });
+});
+
+app.get('/search', (req, res, next) => { 
+    res.render('search-form', { items : [{ value: null }]});
+});
+
+app.post('/search', (req, res, next) => {
+    client.search(req.body.keyword)
+    .then(function (response) {
+      res.render('search-form', response)
+    }).catch(function (err) {
+      throw err;
+    });
+})
+
+
+
+
 app.listen(4000, () => console.log('Running'));
+
