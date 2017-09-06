ed14d33fe90902051266b4af5ccba4817fd3d80e
Mercedes Egido <meegido@gmail.com>
2017-09-06 17:05:31 +0200

    iteration 2

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,22 +1,34 @@
 const express = require('express');
-const app = express();
 const Chuck  = require('chucknorris-io');
+const expressLayout = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 const client = new Chuck();
+const app = express();
 
+app.use(expressLayout)
+app.set('layout', 'index')
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/random', (req, res, next) => {
+
+app.get('/random', (req, response, next) => {
   client.getRandomJoke()
-    .then((res) => {
-      console.log(res.value);
+    .then((resolve) => {
+      response.render ('joke' , {random : resolve})
     }).catch((err) => {
       console.log(err);
     })
-} )
+  })
 
-
+  app.get('/categories', (req, response, next) => {
+    client.getJokeCategories()
+      .then((res) => {
+        response.render ('categories' , {cat : res})
+      }).catch((err) => {
+        console.log(err);
+      })
+    })
 
 let port = 3000;
 app.listen(port, () => {
