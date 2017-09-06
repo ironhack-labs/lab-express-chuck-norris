f9d6dbeca9bf28b75c5f8714d453130287e69215
Luisa Fernanda Salazar Hoyos <luisafernandasalazarhoyos@MacBook-Pro-de-Luisa.local>
2017-09-06 18:37:43 +0200

    hecha hasta iteracion2

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,28 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.get('/random', (req, res) => {
+  client.getRandomJoke()
+    .then((response) => {
+      res.send(`<p>${response.value}</p>`);
+    }).catch((err) => {
+      // handle error
+    });
+
+});
+
+app.get('/categories', (req, res) => {
+  client.getJokeCategories()
+  .then((result)=>  {
+    res.render("categories.ejs", {categories:result} );
+  })
+  .catch((err)=> {
+    // handle error
+  });
+
+});
+
+// Server Started
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!')
+});
