694aac485b40c5711be5f2d07d090c7a8417b665
y4izus <yaiza.garcia.mm@gmail.com>
2017-09-06 21:44:13 +0200

    Iteration 1.- Get a random joke and Iteration 2.- Get a joke by category

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,4 +2,43 @@ const express = require('express');
 const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
+const layouts = require('express-ejs-layouts');
 
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.set(layouts)
+app.set('layout', 'index')
+
+app.get('/random', (req, res) => {
+  client.getRandomJoke().then(function(response) {
+    res.send(`<p>${response.value}</p>`)
+  }).catch(function(err) {
+    // handle error
+  })
+})
+
+app.get('/categories/:cat', (req, res) => {
+  client.getRandomJoke(req.params.cat).then( (response) => {
+    res.render(`joke-by-category`,{
+      joke: response.value
+    })
+  }).catch(function(err) {
+    // handle error
+  })
+})
+
+app.get('/categories', (req, res) => {
+  client.getJokeCategories().then( (response) => {
+    res.render(`categories`,{
+      categories: response
+    })
+  }).catch(function(err) {
+    // handle error
+  })
+})
+
+let port = 3000
+app.listen(port, () => {console.log(`Port ${port}`);})
