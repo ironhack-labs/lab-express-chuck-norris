00f5a352ee79bc89567d95d69067284157f0948a
Mercedes Egido <meegido@gmail.com>
2017-09-06 16:17:21 +0200

    first iteration

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,22 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/random', (req, res, next) => {
+  client.getRandomJoke()
+    .then((res) => {
+      console.log(res.value);
+    }).catch((err) => {
+      console.log(err);
+    })
+} )
+
+
+
+let port = 3000;
+app.listen(port, () => {
+  console.log(`My firs joke listening on por ${port}!`)
+})
