cf12f052dc2ab5ca081b321f767bfb68d1d6362c
VeroGlez <veronicagonzalezperez@MacBook-Pro-de-Veronica.local>
2017-09-06 16:36:56 +0200

    Iteration 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,33 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// our first Route
+app.get('/random', (req,res) =>{
+  // res.send("holi");
+  client.getRandomJoke()
+    .then( (response) => {
+      res.send(`<p>${response.value}</p>`);
+    }).catch((err) => {
+    // handle error
+    });
+
+
+
+});
+
+
+
+
+
+
+
+
+// Server Started
+let port = 3000;
+app.listen(port, () => {
+  console.log(`My first app listening on port ${port}!`);
+});
