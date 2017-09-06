02353f538c5cbce7df11853b95bda0266f5df1a3
ritorrehuelamo <torrehuelamo@gmail.com>
2017-09-06 16:50:10 +0200

    Iteration 1 & 2

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,4 +2,49 @@ const express = require('express');
 const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
+const PORT = 3000
 
+const expressLayout = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+
+app.use(expressLayout)
+app.set('layout', 'index')
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(express.static('public'))
+
+let chuckNorrisCategories,
+	randomJoke
+
+
+app.get('/random', (req, res) => {
+	client.getRandomJoke()
+		.then(res => { randomJoke = res.value })
+		.catch(err => {throw err})
+
+		res.render('random-joke', {
+			randomJokeText: randomJoke 
+		})
+})
+
+app.get('/categories', (req, res) => {
+	client.getJokeCategories().then(res => {
+		chuckNorrisCategories = res
+	}).catch(err => {throw err})
+
+	res.render('categories', {
+		categories: chuckNorrisCategories
+	})
+})
+
+app.get('/', (req, res) => {
+	res.send('Hello World!!')
+})
+
+app.listen(PORT, (req, res) => {
+	console.log(`Express server run on http://localhost:${PORT}`);
+})
