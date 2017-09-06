b27cb29fafc35ddf9f65a792a2561a7b5dcac780
Mark Allen <mark@traversify.com>
2017-09-06 10:11:16 +0200

    Basic file structure

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
+app.use(express.statis('public'));
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', _dirname + "views");
+app.set('layout', 'layout/main-layout');
+app.set('view-engine', 'ejs');
+
+app.get('/', (req, resp, next) => {
+    resp.render('index');
+})
+
+app.get('/random', (req, resp, next) => {
+    resp.render('random');
+})
+
+app.get('/categories', (req, resp, next) => {
+    resp.render('categories');
+})
+
+app.get('/search', (req, resp, next) => {
+    resp.render('search');
+})
 
