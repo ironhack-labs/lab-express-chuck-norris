ef144025d681ae26c9733d3e033991067720fbe1
ritorrehuelamo <torrehuelamo@gmail.com>
2017-09-06 18:11:19 +0200

    Category by href

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const Chuck  = require('chucknorris-io');
+const Chuck = require('chucknorris-io');
 const client = new Chuck();
 const PORT = 3000
 
@@ -13,38 +13,58 @@ app.set('layout', 'index')
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.use(express.static('public'))
 
 let chuckNorrisCategories,
-	randomJoke
+  randomJoke, randomCategory
 
 
 app.get('/random', (req, res) => {
-	client.getRandomJoke()
-		.then(res => { randomJoke = res.value })
-		.catch(err => {throw err})
+  client.getRandomJoke()
+    .then(res => {
+      randomJoke = res.value
+    })
+    .catch(err => {
+      throw err
+    })
 
-		res.render('random-joke', {
-			randomJokeText: randomJoke 
-		})
+  res.render('random-joke', {
+    randomJokeText: randomJoke
+  })
 })
 
 app.get('/categories', (req, res) => {
-	client.getJokeCategories().then(res => {
-		chuckNorrisCategories = res
-	}).catch(err => {throw err})
-
-	res.render('categories', {
-		categories: chuckNorrisCategories
-	})
+  client.getJokeCategories().then(res => {
+    chuckNorrisCategories = res
+  }).catch(err => {
+    throw err
+  })
+  res.render('categories', {
+    categories: chuckNorrisCategories
+  })
 })
 
+
+app.get("/category", (req, res) => {
+  client.getRandomJoke(req.query.cat).then(res => {
+    randomCategory = res
+  }).catch(err => {
+    throw err
+  })
+  res.render("category", {
+    cat: randomCategory
+  })
+})
+
+
 app.get('/', (req, res) => {
-	res.send('Hello World!!')
+  res.send('Hello World!!')
 })
 
 app.listen(PORT, (req, res) => {
-	console.log(`Express server run on http://localhost:${PORT}`);
+  console.log(`Express server run on http://localhost:${PORT}`);
 })
