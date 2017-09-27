4d5c46f3884007ebdae7048e5f54960b9fbe4f3d
Valentin Perot <perot.valentin@gmail.com>
2017-09-27 13:38:51 +0200

    iteration 1 & 2

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,51 @@
 const express = require('express');
-const app = express();
+const expressLayouts = require('express-ejs-layouts');
 const Chuck  = require('chucknorris-io');
+const app = express();
+app.use(expressLayouts);
+
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('layout', 'layout');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res, next) => {
+  res.render('index', {
+    pages: ['Home', 'random', 'categories', 'search']
+  });
+
+})
+
+app.get('/random', (req, res, next) => {
+  // Retrieve a random chuck joke
+client.getRandomJoke()
+  .then((joke) => {
+    console.log(joke);
+    res.render('random',{
+      joke: joke
+    });
+
+  }).catch((err) => {
+    // handle error
+  });
+
+})
+
+app.get('/categories', (req, res, next) => {
+
+  client.getJokeCategories()
+  .then((categories)=>  {
+    console.log(categories);
+    res.render('categories', {
+      categories: categories
+    })
+  })
+  .catch((err)=> {
+    // handle error
+  });
+})
+
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000');
+})
