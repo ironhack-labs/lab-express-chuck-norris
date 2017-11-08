e9c1bc5b967ec2ea8ecc3ab4bb72636551ae5f32
Juan Vallejo-Nagera <juanvallejo-nagera@Juans-MacBook-Pro.local>
2017-11-08 14:19:23 +0100

    iteration2

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,27 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (request, res, next) => {
+  res.render('index');
+});
+
+
+app.get('/random', (req, res, next) => {
+  client.getRandomJoke().then((joke) => {
+      // use the response here
+      res.render('random', {joke :joke.value});
+    });
+});
+
+
+app.get('/categories', (req, res, next) => {
+  client.getJokeCategories()
+  .then((response)=>  {
+      res.render('categories', {response});
+    });
+});
+
+app.listen(3000);
