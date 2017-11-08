0524842182ccb1a88c5819086491a84ba4d672e5
Tess <tessdegregorio@gmail.com>
2017-11-08 16:53:36 +0100

    Until iteration 3, not included

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,49 @@
 const express = require('express');
 const app = express();
-const Chuck  = require('chucknorris-io');
+
+const Chuck = require('chucknorris-io');
 const client = new Chuck();
 
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(express.static("static"))
+
+app.get('/', (request, response, next) => {
+  response.render('index');
+  //response.send('<p>Welcome Ironhacker. :)</p>');
+});
+
+
+app.get("/random", (req, res) => {
+  client.getRandomJoke()
+    .then((joke) => {
+      res.render('random', {
+        joke: joke.value
+      });
+
+    }).catch((err) => {
+      // handle error
+    });
+});
+
+app.get("/categories", (req, res) => {
+  client.getJokeCategories()
+    .then(function(categoriesFromAPI) {
+      res.render('categories', {
+        categories: categoriesFromAPI
+      });
+    }).catch(function(err) {
+      // handle error});
+    });
+});
+
+app.get("/search", (req, res) => {
+  res.render('joke-by-category');
+
+});
+
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!');
+});
