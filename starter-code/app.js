6479abed940d5fb1bf66d01ded5d2b31bbb08031
FranRom <maildefranz@gmail.com>
2017-11-08 14:10:25 +0100

    Iteration 2 done

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,4 +2,41 @@ const express = require('express');
 const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
+app.get("/random", (request, res)=> {
+    client.getRandomJoke()
+      .then((response) => {
+        console.log(response);
+        res.send(`<p>${response.value}</p>`);
+      }).catch((err) => {
+        // handle error
+    });
+ });
+
+app.get("/categories", (request, res)=> {
+  if (request.query.cat === undefined) {
+  client.getJokeCategories()
+    .then((response)=>  {
+      res.render('categories', {categories:response });
+    })
+    .catch((err)=> {
+      // handle error
+    });
+  } else {
+    client.getRandomJoke(request.query.cat)
+  .then((response) => {
+    res.render('joke-by-category', {joke:response.value});
+    console.log(response, request.query.cat);
+    // use the response here
+  }).catch((err) => {
+    // handle error
+  });
+  }
+  });
+
+
+app.listen(3000, () =>{
+  console.log("listen");
+});
