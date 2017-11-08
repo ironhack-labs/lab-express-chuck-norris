82ecb67068fdf472ce203dc7d051c61803593272
J. Gaspar Prieto <jgprieto00@gmail.com>
2017-11-08 14:20:26 +0100

    first commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,58 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const Chuck  = require('chucknorris-io');
+const Chuck = require("chucknorris-io");
 const client = new Chuck();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+//app.use(express.static("views"));
+app.get("/index", (req, res, next) => {
+  res.render("index");
+});
+app.get("/random", (req, res, next) => {
+  client
+    .getRandomJoke()
+    .then(function(response) {
+      res.render("random", response);
+      //console.log(response);
+    })
+    .catch(function(err) {});
+});
+
+app.get("/categories", (req, res, next) => {
+  let cat = {};
+  client
+    .getJokeCategories()
+    .then(function(response) {
+      cat.categs = response;
+      res.render("categories", cat);
+      //console.log(response);
+    })
+    .catch(function(err) {});
+});
+
+// Random categorie quote generator, Bonus!!
+app.get("/rcategories", (req, res, next) => {
+  let joke = {};
+  client
+    .getJokeCategories()
+    .then(function(response) {
+      joke.cat = response;
+      let x = Math.floor(Math.random() * joke.cat.length);
+      //console.log(response);
+      client
+        .getRandomJoke(joke.cat[x])
+        .then(function(response) {
+          response.categories = joke.cat[x];
+          //console.log(response2);
+          res.render("rcategories", response);
+        })
+        .catch(function(err) {});
+    })
+    .catch(function(err) {});
+});
+
+app.listen(3000, () => {
+  console.log("listening");
+});
