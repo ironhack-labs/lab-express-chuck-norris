186771abe546c6be5b7d220824dc12dd91e43df2
Camila Maya <camilamaya21@gmail.com>
2017-11-08 13:54:22 +0100

    First Iteration Complete - Rocio Garcia y Camila Maya

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,26 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(express.static('views'));
+
+app.get('/', (request, response, next) => {
+  response.render('index');
+});
+
+
+app.get('/random', (request, response, next) => {
+    client.getRandomJoke().then((res) => {
+        let chiste = res;
+        response.render('random', chiste);
+  }).catch((err) => {
+    console.log ("Chuck ha muerto.")
+  });
+});
+
+// Server Started
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!')
+});
