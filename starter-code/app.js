0884c4589a7d54bf3d5f3534e0fa7429f6ec0445
Dario Vigil <dariovigil@gmail.com>
2017-11-08 13:08:42 +0100

    Iteration 1.

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,21 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.get('/random', (request, response, next) => {
+  client.getRandomJoke()
+  .then((res) => {
+    // use the response here
+    console.log(res);
+    response.send(`<p>${res.value}</p>`);
+
+  }).catch((err) => {
+    console.log('Error');
+    // handle error
+  });
+  // next();
+});
+
+// Server Started
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!');
+});
