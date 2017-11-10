8f211d25b3ff03a66bc7c0909b8f4cd6c556d30f
Alejandro Roman <aromanarguello@gmail.com>
2017-11-10 11:42:55 -0500

    initial commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,26 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.use( express.static( 'public' ) );
+
+// ROUTES --------------------------
+app.get("/random", (req, res, next) => {
+  client.getRandomJoke()
+    .then((response) => {
+      // use the response here
+      console.log('Response Data:');
+      console.log(response);
+    }).catch((err) => {
+      // handle error
+      console.log('Error Data:');
+      console.log(err);
+    });
+
+    var resVal = response.value;
+
+    res.render("random.ejs");
+});
+
+// END ROUTES-----------------------
+
+app.listen(3000);
