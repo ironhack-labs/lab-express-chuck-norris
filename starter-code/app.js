763f20d5d7ce8d31db788a0fb7c990b5b242b795
Evapavo <32174314+Evapavo@users.noreply.github.com>
2018-01-09 21:10:38 +0100

    Iteration 1 random joke

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,31 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+app.get('/random', (req, res, next) => {
+  client.getRandomJoke()
+    .then((response) => {
+      // use the response here
+      res.send('<p>'+response.value+'</p>');
+    }).catch((err) => {
+      // handle error
+    });
+
+});
+
+app.get('/categories', (req, res, next) => {
+
+});
+
+app.get('/search', (req, res, next) => {
+
+});
+
+
+app.listen(3000);
