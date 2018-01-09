00eda76b0a7d017d06a45ec7a12adb6f3c8a108d
diana-91 <diana.hervasmoya@gmail.com>
2018-01-09 21:04:48 +0100

    Get a random joke

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,16 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.get('/random', (request, response, next) => {
+  client.getRandomJoke()
+  .then((response) => {
+    // use the response here
+    console.log(response);
+
+  }).catch((err) => {
+    // handle error
+    console.log(err)
+  });
+});
+
+app.listen(3000);
