330902d670d1df5a55933187fed57fb3a72a71c6
Juancho Arregui <juan.arregui.lopez@gmail.com>
2018-01-09 21:20:09 +0100

    feat: iteration 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,22 @@
 const express = require('express');
-const app = express();
 const Chuck  = require('chucknorris-io');
+
+const app = express();
 const client = new Chuck();
+
+// our first Route
+app.get('/', (request, response, next) => {
+
+    response.send('<p> </p>');
+  });
+
+
+
+// Retrieve a random chuck joke
+client.getRandomJoke()
+  .then((response) => {
+    // use the response here
+  }).catch((err) => {
+    // handle error
+  });
 
