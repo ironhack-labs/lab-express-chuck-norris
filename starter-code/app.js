c45738f1fe79ab63385994cf588d5af70d7240cb
Jorge Yago <jorge.yago.meniz@gmail.com>
2018-01-09 21:16:35 +0100

    Iteration 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,23 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res, next) => {
+
+  res.render('index');
+});
+
+app.get('/random', (req, res, next) => {
+  // Retrieve a random chuck joke
+client.getRandomJoke()
+  .then((response) => {
+    // use the response here
+    res.render('random', {response});
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
+app.listen(3000);
