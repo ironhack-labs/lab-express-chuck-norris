ee90113eb9514930c650d2f5c828f26b5fd6aa4a
mouyaq <mouyaq@gmail.com>
2018-01-10 21:11:13 +0100

    feat: iteration 1 & 2

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,36 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine','ejs');
+
+app.get('/random', (req, res, next)=>{
+  // Retrieve a random chuck joke
+client.getRandomJoke()
+  .then((response) => {
+    // use the response here
+    //console.log(response);
+    res.render('index', response);
+
+
+  }).catch((err) => {
+    // handle error
+  });
+});
+app.get('/categories', (req, res, next)=>{
+  client.getJokeCategories()
+  .then((response)=>  {
+    // use the response here
+    //console.log(response);
+    res.render('categories', {
+      data: response
+    });
+  })
+  .catch((err)=> {
+    // handle error
+  });
+});
+
+app.listen(3000, () =>{
+  console.log('server started');
+});
