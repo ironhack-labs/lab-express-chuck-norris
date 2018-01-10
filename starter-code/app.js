09e64893cf8e935a53fb342ef2b896abb9b971aa
mouyaq <mouyaq@gmail.com>
2018-01-11 00:16:28 +0100

    feat: iteration 3 done

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,19 +2,20 @@ const express = require('express');
 const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('views', __dirname + '/views');
 app.set('view engine','ejs');
 
-app.get('/random', (req, res, next)=>{
+app.get('/random', (req, res, next) => {
   // Retrieve a random chuck joke
 client.getRandomJoke()
   .then((response) => {
     // use the response here
     console.log(response);
     res.render('index', response);
-
-
   }).catch((err) => {
     // handle error
   });
@@ -45,6 +46,27 @@ app.get('/categories', (req, res, next) => {
     }
 });
 
-app.listen(3000, () =>{
+app.get('/search', (req, res, next) => {
+    res.render('search-form.ejs', { items: [] });
+});
+
+app.post('/search', (req, res, next) => {
+    //console.log(req.body.keyword);
+    if(req.body.keyword) {
+        client.search(req.body.keyword)
+        .then(function (response) {
+            //console.log(response);
+            res.render('search-form.ejs', response);
+        }).catch(function (err) {
+        // handle error
+        });
+    }
+    else {
+        res.render('search-form.ejs', { items: [] });
+    }
+});
+
+
+app.listen(3000, () => { 
   console.log('server started');
 });
