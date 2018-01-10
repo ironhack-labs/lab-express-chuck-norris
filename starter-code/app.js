0a0f8c2b5ac9dc966421ff6fe50a7157480a9878
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-01-10 19:08:02 +0100

    Feat: Random link and random joke category

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,6 +3,9 @@ const app = express();
 const Chuck = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 // our first Route:
 app.get('/random', (request, res, next) => {
   // Retrieve a random chuck joke
@@ -15,6 +18,36 @@ app.get('/random', (request, res, next) => {
       // handle error
     });
 });
+
+app.get('/categories', (request, res, next) => {
+  if(request.query.cat===undefined){
+    client.getJokeCategories()
+      .then((response) => {
+        
+        let data = [];
+        for (let item of response) {
+          let link = "http://localhost:3000/categories?cat=" + item;
+          data.push(link);
+        }
+        let objData = {};
+        objData.links = data;
+        res.render('index', objData);
+      })
+      .catch((err) => {
+        // handle error
+      });
+  }else{
+    client.getRandomJoke(request.query.cat)
+    .then((response) => {
+      // use the response here
+      res.send(`<p>${response.value}</p>`);
+      // res.send(console.log(response));
+    }).catch((err) => {
+      // handle error
+    });
+  }
+});
+
 
 // Server Started
 app.listen(3000, () => {
