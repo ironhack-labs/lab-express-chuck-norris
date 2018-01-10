c92035f313254cbc22f97f2082844e733ce6742f
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-01-10 11:59:09 +0100

    Feat: Iteration 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
-const Chuck  = require('chucknorris-io');
+const Chuck = require('chucknorris-io');
 const client = new Chuck();
 
+// our first Route:
+app.get('/random', (request, res, next) => {
+  // Retrieve a random chuck joke
+  client.getRandomJoke()
+    .then((response) => {
+      // use the response here
+      res.send(`<p>${response.value}</p>`);
+      // res.send(console.log(response));
+    }).catch((err) => {
+      // handle error
+    });
+});
+
+// Server Started
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!');
+});
