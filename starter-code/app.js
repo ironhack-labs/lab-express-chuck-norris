b2d023ad82f21593b38a8b4f9ab338ce2f8f3dfc
Pablo Laso <plasocortabitarte@gmail.com>
2018-01-11 16:49:02 +0100

    feat: add dependencies (layout, express, chucknorris), set a basic layout, index, and random views

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,31 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.use(express.static('public'));
+
+app.use(expressLayouts);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main-layout');
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+app.get('/random', (req, res, next) => {
+  client.getRandomJoke()
+  .then((response) => {
+    console.log(response)
+    res.render('random', {
+      randomJoke: response
+    });
+  }).catch((err) => {
+    console.log("There is an error");
+  });
+});
+
+app.listen(3000);
