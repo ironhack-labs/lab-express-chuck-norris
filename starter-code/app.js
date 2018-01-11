eee513b8fd0acd893f1b28fdccd1745dda541629
pabloonieva <pablo@ironhack.com>
2018-01-11 20:36:47 +0100

    Feat: first iteration

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,17 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+
+// Retrieve a random chuck joke
+app.get('/random', (req,res, next) => {
+  client.getRandomJoke().then(function (response) {
+    // do stuff here
+    res.send(response.value);
+    console.log(response);
+  }).catch(function (err) {
+      // handle error
+      //console.log('Entro en el error');
+      console.log(err);
+  });
+});
+app.listen(3000, () => { console.log("Ready!") });
