48c44bba505916276b8d22a3c4ee5fc7aa541025
Cristina Bertrand <cristina@11870.com>
2018-01-12 23:12:23 +0100

    feat: in the middle of iteration 2

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,34 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res, next) => {
+
+  res.render('index');
+});
+
+app.get('/random', (req, res, next) => {
+  // Retrieve a random chuck joke
+client.getRandomJoke()
+  .then((response) => {
+    // use the response here
+    res.render('random', {response});
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
+app.get('/categories', (req, res, next) => {
+  // Retrieve a random chuck joke
+client.getJokeCategories()
+  .then((response) => {
+    // use the response here
+    res.render('categories', {response});
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
+app.listen(3000);
