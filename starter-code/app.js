73be8cc72c3e6c742e3328ccdb2982324bb99656
Pablo Laso <plasocortabitarte@gmail.com>
2018-01-13 10:09:06 +0100

    feat:random joke completed

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,16 +1,21 @@
+// require modules
+
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
+
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+// Middlewares config
 app.use(express.static('public'));
-
 app.use(expressLayouts);
 
+app.set('layout', 'layouts/main-layout');
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.set('layout', 'layouts/main-layout');
+
+// App routes
 
 app.get('/', (req, res, next) => {
   res.render('index');
@@ -28,4 +33,8 @@ app.get('/random', (req, res, next) => {
   });
 });
 
-app.listen(3000);
+app.get('/categories', (req, res, next) => {
+  res.render('categories');
+});
+
+app.listen(3000, () => console.log("ready!"));
