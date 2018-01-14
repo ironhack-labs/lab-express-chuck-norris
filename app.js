76cdac5d7cf5ab956f3f59dc916ef6fdc37a49bc
Bart Smet <bartsmet30@gmail.com>
2018-01-14 19:09:07 +0100

    feat: Added iteration 2 complete

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const Chuck = require('chucknorris-io');
 const client = new Chuck();
+const bodyParser = require('body-parser');
+
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -15,18 +19,43 @@ app.get('/', (request, response, next) => {
 
 app.get('/random', (request, response, next) => {
   client.getRandomJoke()
-  .then((res) => {
-    response.send(
-    res.value);
-    console.log(res.value);
-  }).catch((err) => {
-    console.log(err);
-  });
+    .then((res) => {
+      response.render('random', {
+        joke: res
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get('/categories', (request, response, next) => {
+  let cat = request.query.cat;
+  if (cat) {
+    client.getRandomJoke(cat)
+      .then((res) => {
+        response.render('joke-by-category', {
+          joke: res,
+          cat: cat
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    client.getJokeCategories()
+      .then((res) => {
+        response.render('categories', {
+          categories: res
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-});
+  }
+
+})
+
 
 app.get('/search', (request, response, next) => {
   response.render('search');
