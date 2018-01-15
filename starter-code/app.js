94702d81dd5d5ba82decc9b80a84be2502c9fcfa
diana-91 <diana.hervasmoya@gmail.com>
2018-01-15 23:14:37 +0100

    update app.js -> categories, categories.ejs and joke-by-category.ejs

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -21,17 +25,31 @@ app.get('/random', (req, res, next) => {
 });
 
 app.get('/categories', (req, res, next) => {
-  client.getJokeCategories()
-  .then((response)=>  {
-    // use the response here
-    res.render('categories');
-  })
-  .catch((err)=> {
-    // handle error
-    console.log(err)
-  });
+  let category = req.query.cat;
+  if(category){
+    client.getRandomJoke(category)
+    .then((response) => {
+      res.render('joke-by-category', {
+        joke: response.value,
+        category: category
+      })
+    }).catch((err) => {
+      // handle error
+      console.log(err)
+    });
+  }else{
+    client.getRandomJoke()
+    .then((response) => {
+      res.render('categories', {
+        categories: response
+      }).catch((err) => {
+        // handle error
+        console.log(err)
+      });
+    });
+  }
 });
 
 
 
-app.listen(3001);
+app.listen(3000);
