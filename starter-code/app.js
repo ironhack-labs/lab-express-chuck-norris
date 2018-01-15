757aea61da168087b3b174a2cf420639e549d2bf
diana-91 <diana.hervasmoya@gmail.com>
2018-01-15 22:32:07 +0100

    index.ejs

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,11 +3,16 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
-app.get('/random', (request, response, next) => {
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/random', (req, res, next) => {
   client.getRandomJoke()
   .then((response) => {
     // use the response here
-    console.log(response);
+    res.render('index', {
+      joke: response
+    });
 
   }).catch((err) => {
     // handle error
@@ -15,4 +20,18 @@ app.get('/random', (request, response, next) => {
   });
 });
 
-app.listen(3000);
+app.get('/categories', (req, res, next) => {
+  client.getJokeCategories()
+  .then((response)=>  {
+    // use the response here
+    res.render('categories');
+  })
+  .catch((err)=> {
+    // handle error
+    console.log(err)
+  });
+});
+
+
+
+app.listen(3001);
