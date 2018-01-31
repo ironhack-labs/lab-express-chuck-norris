834d80c74c853336003dba13101a3d3c7c31cb10
JurgenBlitz <jorge.gopae@gmail.com>
2018-01-31 17:04:12 +0100

    Primera iteración_

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,15 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/random', (req, res, next) => {
+client.getRandomJoke()
+  .then(joke => { let newJoke=joke.value;
+    res.render('index',{name: newJoke });
+  }).catch((err) => {
+    // handle error
+  });
+});
+  app.listen(3000);
