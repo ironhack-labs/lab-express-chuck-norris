e49c03547a2bfcc71f44d3b1065d6a5f2344dc8b
Myriam <bourbiamyriam@gmail.com>
2018-01-31 15:07:20 +0100

    Myriam_Maxim

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,46 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const Chuck  = require('chucknorris-io');
+const Chuck = require("chucknorris-io");
 const client = new Chuck();
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
+app.get("/", (req, res, next) => {
+  console.log(req);
+  res.send("<p>Welcome Ironhacker. :)</p>");
+});
+
+app.get("/random", (req, res, next) => {
+  // Retrieve a random chuck joke
+  client
+    .getRandomJoke()
+    .then(result => {
+      // use the response here
+      console.log("SUCCESS");
+      console.log(result);
+      res.send(`<p>${result.value}</p>`);
+    })
+    .catch(err => {
+      throw err;
+      // handle error
+    });
+});
+
+app.get("/categories", (req, res, next) => {
+  // Retrieve a random chuck joke
+  client
+    .getJokeCategories()
+    .then(result => {
+      res.render("categories", { cat: result });
+      console.log(result);
+      // use the response here
+    })
+    .catch(err => {
+      throw err;
+      // handle error
+    });
+});
+
+app.listen(3000, () => {
+  console.log("My first app listening on port 3000!");
+});
