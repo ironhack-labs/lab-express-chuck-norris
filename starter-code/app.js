936e161c3e3250683a6073a647bbf77cf23c593e
kissy89 <stephanie.senoner@gmail.com>
2018-01-31 14:41:28 +0100

    beginning pair programming

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,19 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+app.get('/', (request, response, next) => {
+    // console.log("method: " + request.method, "path: " + request.path, request.query);
+    response.send('<p>Welcome Ironhacker. :)</p>');
+  });
+
+app.get('/random', (req, res, next) => {
+    res.render('random');
+  });
+
+app.listen(3010, () => {
+    // console.log('My first app listening on port 3000!')
+  });
+
