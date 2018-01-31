954ca8804800fd4a603e21cd5ac30110e09b8f77
Ana Recuero <anarecsa@gmail.com>
2018-01-31 15:59:58 +0100

    express and ejs setup

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const Chuck  = require('chucknorris-io');
+const Chuck = require("chucknorris-io");
 const client = new Chuck();
+const expressLayouts = require("express-ejs-layouts");
 
+
+//app.use(express.static('public'));
+app.use(expressLayouts);
+app.set("layout", "layouts/main-layout");
+app.set('views', __dirname + "/views");
+app.set("view engine","ejs");
+// Retrieve a random chuck joke
+
+// our first Route
+app.get("/random", (request, response, next) => {
+
+//response.send("<h1>HOLA</h1>")
+response.render("index");
+  //console.log(request);
+  // response.send(
+  //   client
+  //   .getRandomJoke()
+  //   .then(joke => {
+  //     console.log(joke);
+  //     response.render("index", joke);
+      
+  //   })
+  //   .catch(err => {
+  //     // handle error
+  //   }));
+  
+});
+
+// Server Started
+app.listen(3000, () => {
+  console.log("My first app listening on port 3000!");
+});
