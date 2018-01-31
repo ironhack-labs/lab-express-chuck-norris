d5bb0029694e24a6ff7e41ec08aba03f7e5af1c1
Lucas Martin Calderon <lucasmartincalderon1@gmail.com>
2018-01-31 17:26:19 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,32 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+app.get('/random', (req, res, next) => {
+  client.getRandomJoke()
+  .then((response) => {
+    res.render('index', {response: response.value});
+  }).catch((err) => {
+    console.log(err)
+  });
+  
+});
+
+app.get('/categories', (req, res, next) => {
+  client.getJokeCategories()
+  .then((response)=>  {
+    let dev = req.query.dev;
+    let sport = req.query.sport;
+    res.render('categories', {response: response.value});
+  })
+  .catch((err)=> {
+    console.log(err);
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Port 300");
+});
