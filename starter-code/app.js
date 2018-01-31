3a7b7893c47f3f8f7678f99647a1d3d22511efee
Susana <susana_vadillo@yahoo.es>
2018-01-31 16:59:18 +0100

    primera iteracion

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,17 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/random', (req, res, next) => {
+  client.getRandomJoke()
+  .then((response) => {
+    res.render('index',{joke: response.value});
+  }).catch((err) => {
+    // handle error
+  });  
+});
+
+app.listen(3000);
+
