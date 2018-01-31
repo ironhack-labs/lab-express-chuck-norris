b687a9524c291e2759a008e576be4d6ccba70e51
Jana Stinn <jana-stinn@gmx.de>
2018-01-31 16:03:10 +0100

    first commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,4 +2,42 @@ const express = require('express');
 const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
+const expressLayouts = require('express-ejs-layouts');
 
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('layout', 'views/index')
+
+app.use(express.static('static'));
+
+app.get('/', (request, response, next) => {
+  response.send('chuck norris');
+});
+
+app.get('/random', (request, response, next) => {
+  client.getRandomJoke().then((data) => {
+    console.log(data);
+    response.render('random', data);
+  }).catch((err) => {
+    console.log('error');
+    response.send('error');
+  });
+});
+
+app.get('/categories', (request, response, next) => {
+  client.getJokeCategories().then((data) => {
+    console.log(data);
+    let jokeCat = {
+      array : data
+    };
+    response.render('categories', jokeCat);
+  }).catch((err) => {
+    console.log('error');
+    response.send('error');
+  });
+});
+
+app.listen(3000, () => {
+  console.log('chuck norris');
+})
