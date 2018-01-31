cec152b67951a970fc9d713e89f092ad0f6a39b6
Ana Recuero <anarecsa@gmail.com>
2018-01-31 16:11:42 +0100

    iteration 1 completed

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,33 +4,30 @@ const Chuck = require("chucknorris-io");
 const client = new Chuck();
 const expressLayouts = require("express-ejs-layouts");
 
-
 //app.use(express.static('public'));
 app.use(expressLayouts);
 app.set("layout", "layouts/main-layout");
-app.set('views', __dirname + "/views");
-app.set("view engine","ejs");
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 // Retrieve a random chuck joke
 
-// our first Route
 app.get("/random", (request, response, next) => {
-
-//response.send("<h1>HOLA</h1>")
-response.render("index");
-  //console.log(request);
-  // response.send(
-  //   client
-  //   .getRandomJoke()
-  //   .then(joke => {
-  //     console.log(joke);
-  //     response.render("index", joke);
-      
-  //   })
-  //   .catch(err => {
-  //     // handle error
-  //   }));
   
+  client
+  .getRandomJoke()
+  .then(joke => {
+    console.log(joke);
+    response.render("index", { joke: joke });
+  })
+  .catch(err => {
+    console.log("Error retrieving joke")
+  })
+
 });
+
+// our first Route
+
+
 
 // Server Started
 app.listen(3000, () => {
