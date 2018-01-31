17b5eba388aa35223f4fba7f4b57e5d0d16d5fab
Oscar Jorge <oscar.jorge.jorge@hotmail.com>
2018-01-31 16:31:51 +0100

    learning

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,31 @@
 const express = require('express');
 const app = express();
 const Chuck  = require('chucknorris-io');
+
+//configure app
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 const client = new Chuck();
+
+
+//routes
+app.get('/index', (req, res, next) => {
+ client.getRandomJoke().
+  then((response) => {
+    let data = {
+      joke: response.value
+      
+    };
+    res.render('index', data);
+   }).catch((err) => {
+    console.log(err);
+   });
+});
+
+//start app
+app.listen(3001, () => {
+  console.log('Easy web dev. 3001!')
+});
+
 
