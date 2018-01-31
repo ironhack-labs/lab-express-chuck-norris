4c0f5d6814b7de26b893e1dcc5360a4e98aa0d83
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-01-31 16:03:19 +0100

    only finished number one

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,56 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) =>{
+    res.render('index');
+});
+
+
+app.get('/random', (req, res) => {
+  client
+    .getRandomJoke()
+    .then(response => {
+        let data = {
+            value: response.value
+        }
+        res.render('index', data);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+})
+
+app.get('/categories', (req, res) => {
+    client
+      .getJokeCategories()
+      .then((response) => {
+          let data = {
+              value: response
+          }
+          res.render('index', data);
+      })
+      .catch(err => {
+          console.log(err);
+      })
+})
+
+
+app.listen(3001, () => {
+    console.log('we made it');
+})
+
+
+
+
+// res.send(
+//     `<p> res.value </p>` );
+
+// let data = {
+//     value = client.getRandomJoke()
+// }
