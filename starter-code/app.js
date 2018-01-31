ce54d15ab17583bfb243a0b57d2ec3d54a1ea35f
ngoc0905 <lebichngoc090589@gmail.com>
2018-01-31 15:07:53 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,33 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+app.get("/random", (req, res, next) => {
+    client.getRandomJoke()
+  .then((response) => {
+     // console.log(response);
+    res.send(`<p>${response.value}</p>`);
+  }).catch((err) => {
+      console.log(err);
+    res.send(err);
+  });
+});
+
+app.get("/categories", (req, res, next) => {
+    client.getJokeCategories()
+  .then((response)=>  {
+    console.log(response);  
+    //res.send(`<p>${response}</p>`);
+    res.render("categories",{lapin: response});
+  })
+  .catch((err)=> {
+    console.log(err);
+    res.send(err);
+  });
+});
+// app.get("/search", (req, res, next) => {});
+app.listen(3000, () => {
+    console.log("I'm ready!");
+  });
