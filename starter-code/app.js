59452a946e51f43074f383eb60aae7bbb3180aed
Sébastien Del Mestre <sebastien.delmestre@gmail.com>
2018-01-31 19:54:38 +0100

    done 2nd iteration

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,39 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+/*
+app.get('/', (req,res) => {
+  let hola = "manuel"
+  res.render('index', {name: hola});  
+});
+*/
+
+app.get('/random', (req,res) => {
+  client.getRandomJoke()
+  .then((response) => {
+    // use the response here
+    res.render('index', {joke:response.value});   
+  }).catch((err) => {
+    // handle error
+  });  
+});
+
+app.get('/categories', (req,res) => {
+client.getJokeCategories()
+  .then((response)=>  {
+    var x = '';
+    for (var i = 0; i<response.length; i++) {
+       x += (response[i] + '')
+    };
+    res.render('index', {categories: x})
+    res.render('categories',{categories: response});
+  }).catch((err)=> {
+    // handle error
+  }); 
+  });
+
+
+
+app.listen(3000);
