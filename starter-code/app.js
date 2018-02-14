6867bece84f9d39a931242bf8b09e4a8640ba0db
Pablo Gomez <gomezpa@bellsouth.net>
2018-02-14 15:53:50 -0500

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,36 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+
+
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+app.get('/random', (req, res, next) => {
+// Retrieve a random chuck joke
+client.getRandomJoke()
+  .then((response) => {
+    res.render("random", { joke: response.value });
+    // use the response here
+  }).catch((err) => {
+    // handle error
+  });
+ 
+  });
+
+  app.get('/categories', (req, res, next) => {
+    client.getJokeCategories()
+    .then((response)=>  {
+        res.render("categories", { categories: response });
+      // use the response here
+    })
+    .catch((err)=> {
+      // handle error
+    });
+     
+      });
+
+  app.listen(3000, () => {
+    console.log("My first app listening on port 3000!");
+});
